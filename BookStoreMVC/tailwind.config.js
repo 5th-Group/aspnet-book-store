a9cb09658c11fd18c8d07ef6e5997217433c6283
nbi67 <nbi67@NYNEO>
2022-11-23 21:53:27 +0700

    Enhance Footer and Tailwind  Config

--- BookStoreMVC/tailwind.config.js
+++ BookStoreMVC/tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
     extend: {
       colors: {
         primary: "#34979D",
+        primary2: {
+          100: "#d6eaeb",
+          200: "#aed5d8",
+          300: "#85c1c4",
+          400: "#5dacb1",
+          default: "#34979d",
+          600: "#2a797e",
+          700: "#1f5b5e",
+          800: "#153c3f",
+          900: "#0a1e1f",
+        },
         titleText: "#4D4D4D",
         normalText: "#8B8B8B",
         fade: "#C5C5C5",
