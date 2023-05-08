e71db1bfcf393339c3135994f942e35f565f2231
Wirex29 <62797062+Wirex29@users.noreply.github.com>
2023-05-08 11:50:00 +0700

    Update BookStoreDataAccess.cs

--- BookStoreMVC/DataAccess/BookStoreDataAccess.cs
+++ BookStoreMVC/DataAccess/BookStoreDataAccess.cs
@@ -5,18 +5,7 @@ namespace BookStoreMVC.DataAccess;
 
 public class BookStoreDataAccess
 {
-        // private const string ConnectionString =  "mongodb+srv://admin:090327298@cluster0.j8vge.mongodb.net/?retryWrites=true&w=majority";
-        // private const string DatabaseName = "BookStore";
-        // private const string BookCollectionName = "Books";
-        // private const string AuthorCollectionName = "Authors";
-        // private const string BookGenreCollectionName = "BookGenres";
-        // private const string BookTypeCollectionName = "BookTypes";
-        // private const string CountryCollectionName = "Countries";
-        // private const string LanguageCollectionName = "Languages";
-        // private const string OrderCollectionName = "Orders";
-        // private const string ProductCollectionName = "Products";
-        // private const string PublisherCollectionName = "Publishers";
-        // private const string ReviewCollectionName = "Reviews";
+
 
         public string ConnectionString { get; set; }
         public string DatabaseName { get; set; }
